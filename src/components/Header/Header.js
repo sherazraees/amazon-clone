c577e741cf792b4e72c6f18ca4c007050e1b971f
sherazraees <sherazraees3@gmail.com>
2023-02-18 13:22:12 +0500

    Header Component Start

---
+++ src/components/Header/Header.js
@@ -0,0 +1,37 @@
+import React from "react";
+import "./Header.css";
+import SearchIcon from "@mui/icons-material/Search";
+function Header() {
+  return (
+    <div className="header">
+      <img
+        className="headerImg"
+        src="https://images-na.ssl-images-amazon.com/images/G/01/gno/images/general/backup-logo_blue._CB481621655_.png"
+      />
+
+      <div className="headerSearch">
+        <input className="headerSearchInput" typpe="text" />
+        <SearchIcon className="headerSearchIcon" />
+      </div>
+
+      <div className="headerNav">
+        <div className="headerOption">
+          <span className="headerOptionLineOne">Hello Guest</span>
+          <span className="headerOptionLineTwo">Sign In</span>
+        </div>
+
+        <div className="headerOption">
+          <span className="headerOptionLineOne">Returns</span>
+          <span className="headerOptionLineTwo">Orders</span>
+        </div>
+
+        <div className="headerOption">
+          <span className="headerOptionLineOne">Your</span>
+          <span className="headerOptionLineTwo">Prime</span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCsPH5YS7TjeZA_0XPMnWc9Ob7xSYI60bg",
+  authDomain: "clone-1a7ae.firebaseapp.com",
+  projectId: "clone-1a7ae",
+  storageBucket: "clone-1a7ae.appspot.com",
+  messagingSenderId: "1076177166713",
+  appId: "1:1076177166713:web:45a95f38f7b78c34f78fa8",
+  measurementId: "G-2J75N9H92C",
+};
